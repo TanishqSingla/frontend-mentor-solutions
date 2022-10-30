fa31c2cc4b985dd9a6abec7eab241e3fc83a6a1f
TanishqSingla <tanishqsingla00@gmail.com>
2022-10-30 20:05:29 +0530

    Complete header

--- ecommerce-product-page-main/src/components/Header/Header.jsx
+++ ecommerce-product-page-main/src/components/Header/Header.jsx
@@ -1,32 +1,38 @@
-import Cart from '../../icons/cart';
-import Menu from '../../icons/Menu';
-import style from './Header.module.css';
+import Cart from "../../icons/cart";
+import Menu from "../../icons/Menu";
+import style from "./Header.module.css";
 
 export default function Header() {
-  return <div className={style.Header}>
-    <div className={style.Menu}>
-      <Menu />
-    </div>
-    <div className={style.Logo}>
-      <h1>sneakers</h1>
-    </div>
-    <div className={style.Socials}>
-      <nav>
-        <ul>
-          <li>Collections</li>
-          <li>Men</li>
-          <li>Women</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </div>
-    <div style={{ flexGrow: 1 }} />
-    <div className={style.Cart}>
-      <Cart />
-    </div>
-    <div className={style.Avatar}>
-      <img src="../../../images/image-avatar.png" alt='pfp' style={{ height: '1.5rem', width: '1.5rem' }} />
-    </div>
-  </div>
+	return (
+		<div className={style.Header}>
+    <div className={style.Container}>
+			<button className={style.Menu}>
+				<Menu />
+			</button>
+			<div className={style.Logo}>
+				<h1>sneakers</h1>
+			</div>
+			<div className={style.Socials}>
+				<nav>
+					<ul>
+						<li>Collections</li>
+						<li>Men</li>
+						<li>Women</li>
+						<li>About</li>
+						<li>Contact</li>
+					</ul>
+				</nav>
+			</div>
+			<div style={{ flexGrow: 1 }} />
+			<div className={style.Cart}>
+				<Cart />
+			</div>
+			<img
+				src="../../../images/image-avatar.png"
+				alt="pfp"
+				className={style.Avatar}
+			/>
+		</div>
+</div>
+	);
 }
