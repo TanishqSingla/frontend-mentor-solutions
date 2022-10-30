fe3a309bd3410e374511b6f04b06536b75a385a6
TanishqSingla <tanishqsingla00@gmail.com>
2022-10-30 19:10:07 +0530

    make header responsive

--- ecommerce-product-page-main/src/components/Header/Header.jsx
+++ ecommerce-product-page-main/src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
+import Cart from '../../icons/cart';
+import Menu from '../../icons/Menu';
 import style from './Header.module.css';
 
 export default function Header() {
   return <div className={style.Header}>
+    <div className={style.Menu}>
+      <Menu />
+    </div>
     <div className={style.Logo}>
       <h1>sneakers</h1>
     </div>
@@ -15,6 +20,13 @@ export default function Header() {
           <li>Contact</li>
         </ul>
       </nav>
+    </div>
+    <div style={{ flexGrow: 1 }} />
+    <div className={style.Cart}>
+      <Cart />
+    </div>
+    <div className={style.Avatar}>
+      <img src="../../../images/image-avatar.png" alt='pfp' style={{ height: '1.5rem', width: '1.5rem' }} />
     </div>
   </div>
 }
